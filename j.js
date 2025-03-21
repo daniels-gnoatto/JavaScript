cd659e2b1c9340ffb83c29347a0218d6b3407716
Daniels <daniel_gnoatto@estudante.sesisenai.org.br>
2025-03-21 10:01:00 -0300

    Fiz da 4 até a 9 da atividade

---
+++ j.js
@@ -0,0 +1,152 @@
+/*
+
+// Atividade 1 
+
+let numero = prompt('Digite o seu número:');
+
+function verificarPar(numero){
+    if(numero % 2 === 0){
+        console.log(numero + "é igual a par.");
+    }
+    else{
+        console.log(numero + "é igual a impar.");
+    }
+}
+
+//verificarPar(numero);
+/*
+
+
+
+
+
+/*
+
+// Atividade 2
+
+let numero1 = parseFloat(prompt("Digite o número que você quer calcular"));
+let numero2 = parseFloat(prompt("Digite o número que você quer calcular"));
+
+function calcular(numero1, numero2){
+    const soma = numero1 + numero2
+    const subtracao = numero1 - numero2
+    const divisao = numero1 / numero2
+    const multiplicacao = numero1 * numero2
+
+    console.log (" A soma dos numeros é igual:" + soma)
+    console.log (" A subtracao dos numeros é igual:" + subtracao)
+    console.log (" A divisao dos numeros é igual:" + divisao)
+    console.log (" A multiplicacao dos numeros é igual:" + multiplicacao)
+    
+}
+
+calcular(numero1, numero2);
+*/
+
+
+
+/*
+
+// Atividade 3 
+
+let numero = 10
+
+while(numero >= 1 ) {
+    console.log (numero)
+    numero--
+}
+*/
+
+
+/*
+// Atividade 4
+
+let string = (prompt('Teste'));
+
+function InverterTexto(string) {
+    let trocar = (string.split('').reverse().join(''));
+    console.log(trocar);
+}
+InverterTexto(string);
+*/
+
+/*
+// Atividade 5
+
+let string = (prompt('Teste'));
+
+function QtdLetras (string) {
+    let Qtd = string.length;
+    console.log (Qtd)
+
+}
+
+QtdLetras(string)
+*/
+
+/*
+// Atividade 6
+
+const carro = {
+    Marca: 'Pagani',
+    Modelo: 'Utopia',
+    Ano: '2022'
+
+};
+
+console.log('O carro é um ' + carro.Marca + ' do modelo ' + carro.Modelo + ' do ano ' + carro.Ano);
+*/
+
+/*
+// Atividade 7
+let nome = (prompt('Digite o nome da pessoa que você quer enviar o texto'));
+const mensagem = 'Olá!'
+
+function mensagemPersonalizada (nome, mensagem) {
+    console.log(mensagem + nome)
+}
+
+mensagemPersonalizada (nome, mensagem)
+*/
+
+/*
+// Atividade 8
+let numero1 = parseFloat(prompt("Digite o número que você quer calcular"));
+let numero2 = parseFloat(prompt("Digite o número que você quer calcular"));
+let numero3 = parseFloat(prompt("Digite o número que você quer calcular"));
+
+function calcular(numero1, numero2, numero3){
+    const soma = numero1 + numero2 + numero3
+    const media = soma / 3
+
+    console.log ("Sua média é de " + media)
+
+}
+
+calcular(numero1, numero2, numero3)
+*/
+
+
+
+// Atividade 9
+/*
+numero = 0
+
+while(numero < 20 ) {
+    numero++
+    let resultado = numero % 3
+
+    if (resultado === 0) {
+        console.log (numero + ' é multiplo de 3')
+    }
+    
+    else {
+        console.log (numero + ' não é multiplo de 3')
+        
+}
+}
+*/
+
+    
+
+
